b68c239117aebd6914c7ddd18986379eed068f17
razouk <houssam.razouk@infineon.com>
2024-11-04 09:18:04 +0100

    handling  cases where

--- transforming_csv_to_csv.py
+++ transforming_csv_to_csv.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
 
-df=pd.read_csv('data/input_data_evaluation_en_bert-base-cased_BIO_annotation.csv')
+df=pd.read_csv('data/input_data_evaluation_en_bert-base-cased_BIN_annotation.csv')
 
-df.to_csv('data/input_data_evaluation_en_bert-base-cased_BIO_annotation_corrected.csv',index=False,sep=';')
+for index,row in df.iterrows():
+
+    if str(row['Answer'])=='nan':
+        
+        print (row)
+        df['Answer'][index]="No Answer"
+
+df.to_csv('data/input_data_evaluation_en_bert-base-cased_BIN_annotation_corrected.csv',index=False,sep=';')
+
+
+df['ID'][228]
+str(df['Answer'][228])
