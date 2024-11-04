557e99f502485d36d5ba47651e07aa2587408b4a
razouk <houssam.razouk@infineon.com>
2024-11-04 18:35:34 +0100

    training on the combined data

--- src/Model_vanilla_seq_tagger_BIN/config.py
+++ src/Model_vanilla_seq_tagger_BIN/config.py
@@ -4,9 +4,11 @@ def init():
     config['LM']='bert-base-multilingual-cased'
     config['max_length']=[64,128,256,512,1024]
     #config['input_train_data_file']='training_data_en'
-    config['input_train_data_file']='training_data_es'
+    #config['input_train_data_file']='training_data_es'
+    config['input_train_data_file']='training_data_en&es'
     #config['input_test_data_file']='reference_data_practice_en'
-    config['input_test_data_file']='reference_data_practice_es'
+    #config['input_test_data_file']='reference_data_practice_es'
+    config['input_test_data_file']='reference_data_practice_en&es'
     config['folds']=[420,200,100,150,24]
 
     return config
