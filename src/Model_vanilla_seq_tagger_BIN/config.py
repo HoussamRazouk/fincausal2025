062848466c41daafe8929b79eec884c7c2c8b45f
razouk <houssam.razouk@infineon.com>
2024-11-04 18:03:01 +0100

    created the data set ready to train  BERT ML es

--- src/Model_vanilla_seq_tagger_BIN/config.py
+++ src/Model_vanilla_seq_tagger_BIN/config.py
@@ -3,8 +3,10 @@ def init():
     #config['LM']='bert-base-cased'
     config['LM']='bert-base-multilingual-cased'
     config['max_length']=[64,128,256,512,1024]
-    config['input_train_data_file']='training_data_en'
-    config['input_test_data_file']='reference_data_practice_en'
+    #config['input_train_data_file']='training_data_en'
+    config['input_train_data_file']='training_data_es'
+    #config['input_test_data_file']='reference_data_practice_en'
+    config['input_test_data_file']='reference_data_practice_es'
     config['folds']=[420,200,100,150,24]
 
     return config
