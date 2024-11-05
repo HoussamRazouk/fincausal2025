b30a2fb898171ef6c0d23975134a391ed28bf45a
razouk <houssam.razouk@infineon.com>
2024-11-05 14:53:23 +0100

    final inference

--- src/Model_vanilla_seq_tagger_BIN/Final_inference.py
+++ src/Model_vanilla_seq_tagger_BIN/Final_inference.py
@@ -14,25 +14,38 @@ from src.Model_vanilla_seq_tagger_BIN.predict_example import predict_an_example
 import random
 
 
-config=init()
-LM_name='bert-base-cased'
-Testing_file='input_data_evaluation_es'
+#config=init()
+LM_names=['bert-base-multilingual-cased_en&esLast training_model','bert-base-multilingual-cased_enLast training_model','bert-base-multilingual-cased_esLast training_model','bert-base-spanish-wwm-cased_esLast training_model']
+tokenizer_names=['bert-base-multilingual-cased','bert-base-multilingual-cased','bert-base-multilingual-cased','dccuchile/bert-base-spanish-wwm-cased']
 
-Test_data,Test_data_maxlength=get_test_data(Testing_file,LM_name)
-for max_length in config['max_length']: ## specify the max_length based on the training data
-            if Test_data_maxlength<max_length:
-                Test_data_maxlength=max_length
-                break
+Testing_files=['input_data_evaluation_es','input_data_evaluation_en']
 
-tokenizer = BertTokenizer.from_pretrained(LM_name)
 
-model_weights=f"src/Model_vanilla_seq_tagger_BIN/trained_models/bert-base-cased_BIN/bert-base-cased_Last training_model.pth"
-#src/Model_vanilla_seq_tagger_BIN/trained_models/bert-base-cased_BIN/bert-base-cased_420_model .pth
-model=torch.load(model_weights,map_location=torch.device('cpu') )
-model.eval()
+#for max_length in config['max_length']: ## specify the max_length based on the training data
+#            if Test_data_maxlength<max_length:
+#                Test_data_maxlength=max_length
+#                break
 
-Test_data['Answer']=Test_data.apply(lambda row: predict_an_example(row,tokenizer,model,max_length=512), axis=1)
-Test_data['Answer']=Test_data.apply(lambda row: row['Answer'][1], axis=1)
 
-Test_data[['ID','Text','Question','Answer']].to_csv('data/input_data_evaluation_en_bert-base-cased_BIN_annotation_es.csv',index=False,sep=';')
+for Testing_file in Testing_files:
+    i=0
+    for LM_name in LM_names:
+        Test_data,Test_data_maxlength=get_test_data(Testing_file,tokenizer_names[i])
+
+        tokenizer = BertTokenizer.from_pretrained(tokenizer_names[i])
+        if LM_name== 'bert-base-spanish-wwm-cased_esLast training_model':
+
+            model_weights=f"src/Model_vanilla_seq_tagger_BIN/trained_models/bert-base-spanish-wwm-cased_BIN/{LM_name}.pth"
+        else:
+            model_weights=f"src/Model_vanilla_seq_tagger_BIN/trained_models/bert-base-multilingual-cased_BIN/{LM_name}.pth"
+        #src/Model_vanilla_seq_tagger_BIN/trained_models/bert-base-cased_BIN/bert-base-cased_420_model .pth
+        
+        model=torch.load(model_weights,map_location=torch.device('cpu') )
+        model.eval()
+
+        Test_data['Answer']=Test_data.apply(lambda row: predict_an_example(row,tokenizer,model,max_length=512), axis=1)
+        Test_data['Answer']=Test_data.apply(lambda row: row['Answer'][1], axis=1)
+
+        Test_data[['ID','Text','Question','Answer']].to_csv(f'data/{Testing_file}_{LM_name[:-19]}.csv',index=False,sep=';')
+        i=i+1
 
--- transforming_csv_to_csv.py
+++ transforming_csv_to_csv.py
@@ -1,17 +1,45 @@
 import pandas as pd
+import pandas as pd
+import zipfile
+import io
 
+LM_names=['bert-base-multilingual-cased_en&esLast training_model','bert-base-multilingual-cased_enLast training_model','bert-base-multilingual-cased_esLast training_model','bert-base-spanish-wwm-cased_esLast training_model']
+f_names=['BERT_ML_EN&ES','BERT_ML_EN','BERT_ML_ES','BERT_ES_ES']
 
-df=pd.read_csv('data/input_data_evaluation_en_bert-base-cased_BIN_annotation.csv')
+Testing_files=['input_data_evaluation_es','input_data_evaluation_en']
 
-for index,row in df.iterrows():
+for Testing_file in Testing_files:
+    i=0
+    
+    for LM_name in LM_names:
 
-    if str(row['Answer'])=='nan':
-        
-        print (row)
-        df['Answer'][index]="No Answer"
+        df=pd.read_csv(f'data/{Testing_file}_{LM_name[:-19]}.csv',sep=';')
 
-df.to_csv('data/input_data_evaluation_en_bert-base-cased_BIN_annotation_corrected.csv',index=False,sep=';')
+        for index,row in df.iterrows():
 
+            if str(row['Answer'])=='nan':
+                
+                print (row)
+                df['Answer'][index]="No Answer"
 
-df['ID'][228]
-str(df['Answer'][228])
+        # Create a ZipFile object
+        if Testing_file=='input_data_evaluation_es':
+            file_name='Es_on_'+f_names[i]
+        else:
+            file_name='En_on_'+f_names[i]
+        with zipfile.ZipFile(f'zips/{file_name}.zip', 'w') as zip_file:
+            # Create a BytesIO object to write the CSV data to
+            csv_buffer = io.BytesIO()
+            
+            # Write the DataFrame to the CSV buffer
+            df.to_csv(csv_buffer, index=False,sep=';')
+            
+            # Write the CSV buffer to the zip file
+            if Testing_file=='input_data_evaluation_es':
+                
+                zip_file.writestr('results_es.csv', csv_buffer.getvalue())
+            else :
+                zip_file.writestr('results_en.csv', csv_buffer.getvalue())
+                
+        i=i+1
+
